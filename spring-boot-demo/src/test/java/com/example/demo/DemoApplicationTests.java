ea8444b4ccfb10a662735e6154d2180b38b00ffc
huangwy <huangwy@2345.com>
2017-07-27 09:21:15 +0800

    ### 集成mysql

--- spring-boot-demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-boot-demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,26 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Test
 	public void contextLoads() {
+		List<Map<String,Object>> result = jdbcTemplate.queryForList("select * from ay_test");
+		System.out.println("query result is" + result.size());
+		System.out.println("success");
 	}
 
 }
