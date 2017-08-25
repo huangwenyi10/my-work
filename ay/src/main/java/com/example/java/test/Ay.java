de599563fbaaa98f8b27f3b65944ab17e624943f
Ay <wenyi.huang@e-vada.com>
2017-08-25 10:48:25 +0800

    【整合技术】 --->>> java.test create

---
+++ ay/src/main/java/com/example/java/test/Ay.java
@@ -0,0 +1,20 @@
+package com.example.java.test;
+
+/**
+ * Created by Ay on 2017/8/24.
+ */
+public class Ay {
+
+    public static void main(String[] args) {
+
+//        final String str1 = "c"; //final类型的变量在编译时当常量处理
+//        String str2 = "ab" + "c";
+//        String str3 = "ab" + str1;
+//        System.out.println(str2==str3);
+
+        String str1 = "c";
+        String str2 = "ab" + "c";
+        String str3 = "ab" + str1;
+        System.out.println(str2==str3);
+    }
+}
