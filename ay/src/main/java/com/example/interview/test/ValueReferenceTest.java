aa46089386a0477271f8fd2ccf0237181b52a6a8
Ay <wenyi.huang@e-vada.com>
2017-09-22 23:07:02 +0800

    【面试题】 --->>> 值类型和引用类型的区别

--- ay/src/main/java/com/example/interview/test/ValueReferenceTest.java
+++ ay/src/main/java/com/example/interview/test/ValueReferenceTest.java
@@ -8,14 +8,17 @@ public class ValueReferenceTest {
         public static void main(String[] args) {
             //内存划分为栈内存(小)和堆内存(大)
             // 值类型
-            int i = 10;//保存在栈内存
-            int m = i;//把值复制给m
+            //保存在栈内存
+            int i = 10;
+            //把值复制给m
+            int m = i;
             i = 11;
             System.out.println("m=" + m + "   /  " + "i=" + i);
-            //引用类型；除了基本数据类型，数组，String，类
+            //引用类型；除了基本数据类型。数组，String，类
             //实际内容存在堆，栈里只存对应到堆的地址
             int[] a = new int[]{1,2,3,4};
-            int[] b = a;//把对应到堆的地址给了b
+            //把对应到堆的地址给了b
+            int[] b = a;
             a[2] = 100;
             System.out.println("b[2]=" + b[2]);
         }
