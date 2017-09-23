b7c6b20dff944f07b677b30af912b167e5388c35
Ay <wenyi.huang@e-vada.com>
2017-09-23 09:02:10 +0800

    【面试题】 --->>> git test

---
+++ ay/src/main/java/com/example/interview/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.example.interview.test;
+
+/**
+ * Created by Ay on 2017/9/23.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+}
