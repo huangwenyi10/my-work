fe674b4555f9209576db76a0c81b3ed3d5a7664c
Ay <wenyi.huang@e-vada.com>
2017-10-22 22:56:57 +0800

    【java工作经验总结】 --->>> Lists.partition使用

--- ay/src/main/java/com/example/java/toolkit/guava/Lists_Partition.java
+++ ay/src/main/java/com/example/java/toolkit/guava/Lists_Partition.java
@@ -1,4 +1,4 @@
-package com.example.java.toolkit.guava.Lists;
+package com.example.java.toolkit.guava;
 
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
@@ -14,10 +14,10 @@ import java.util.List;
  * @author Ay
  * @date 2017/10/22
  */
-public class partition {
+public class Lists_Partition {
 
 
-    Logger log = LoggerFactory.getLogger(partition.class);
+    Logger log = LoggerFactory.getLogger(Lists_Partition.class);
 
 
     public static void main(String[] args) {
@@ -27,25 +27,18 @@ public class partition {
 
         List<List<Integer>> partList = Lists.partition(numList, 3);
         if (!CollectionUtils.isEmpty(partList)) {
-
             for (List<Integer> list : partList) {
-
                 System.out.println(list.toString());
             }
-
         }
 
         //step.2 切割数量大于集合数量
         List<Integer> numList2 = Lists.newArrayList(1);
-
         List<List<Integer>> partList2 = Lists.partition(numList2, 3);
         if (!CollectionUtils.isEmpty(partList2)) {
-
             for (List<Integer> list : partList2) {
-
                 System.out.println(list.toString());
             }
-
         }
 
 
@@ -54,19 +47,13 @@ public class partition {
 
         List<List<Integer>> partList3 = Lists.partition(numList3, 3);
         if (!CollectionUtils.isEmpty(partList3)) {
-
             for (List<Integer> list : partList3) {
-
                 for(int i=0,len = list.size();i<len;i++){
                     list.set(i,8);
-
                 }
             }
             //打印原集合
-            for (List<Integer> list : partList3) {
-
-                System.out.println(list.toString());
-            }
+            System.out.println(numList3.toString());
 
         }
 
@@ -75,7 +62,7 @@ public class partition {
         //step.4 List.partition（）在真实项目中的运用
         List<Integer> numList4 = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        List<List<Integer>> partList4 = Lists.partition(numList4, 3);
+        List<List<Integer>> partList4 = Lists.partition(numList4, 100);
         if (!CollectionUtils.isEmpty(partList4)) {
 
             for (List<Integer> list : partList4) {
